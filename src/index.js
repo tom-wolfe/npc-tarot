bd37557c5802f5448a17d94fc07dca550763e82b
Tom Wolfe <trwolfe13@gmail.com>
2018-06-06 08:38:00 +0100

    Add basic major arcana list.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world');
+const Random = require('random-js');
+
+const random = new Random(Random.engines.mt19937().autoSeed());
+const cards = require('./cards.json');
+
+random.shuffle(cards);
+
+console.log(cards);
