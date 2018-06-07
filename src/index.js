35ea40b8c4253d5e6449e22666bfac69a03f0fe0
Tom Wolfe <trwolfe13@gmail.com>
2018-06-07 10:24:57 +0100

    Convert to web app.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,12 @@
 const performReading = require('./cards/perform-reading');
+const formatReading = require('./cards/format-reading');
 
-function describeCard(card) {
-  let name = '';
-  if (card.suit.toUpperCase() === 'MAJOR') {
-    name = card.name;
-  } else {
-    name = `${describeNumber(card.number)} of ${card.suit}`;
-  }
-  if (card.reversed) { name += ' (reversed)'; }
-  return name;
+function printReading(e) {
+  const reading = performReading();
+  document.getElementById('reading').innerHTML = formatReading(reading);
+  if (e) { e.preventDefault(); }
+  return false;
 }
+document.getElementById('generate').addEventListener('click', printReading);
 
-function describeNumber(number) {
-  switch (number) {
-    case 1: return 'Ace';
-    case 2: return 'Two';
-    case 3: return 'Three';
-    case 4: return 'Four';
-    case 5: return 'Five';
-    case 6: return 'Six';
-    case 7: return 'Seven';
-    case 8: return 'Eight';
-    case 9: return 'Nine';
-    case 10: return 'Ten';
-    case 11: return 'Page';
-    case 12: return 'Knight';
-    case 13: return 'Queen';
-    case 14: return 'King';
-    default: throw new Error(`Unexpected number: ${number}.`);
-  }
-}
-
-const reading = performReading();
-reading.forEach(r => {
-  const description = r.card.reversed ? r.card.reverseDescription : r.card.uprightDescription;
-  console.log(`${r.meaning}: ${describeCard(r.card)}\n${description}\n`);
-});
+printReading();
